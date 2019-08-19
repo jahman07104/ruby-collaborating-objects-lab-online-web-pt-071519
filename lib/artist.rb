73308ec35cace7ca3cc50e1fcbad0d2900adc468
Patrick Harrison <jahman@bellatlantic.net>
2019-08-19 23:33:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -39,7 +39,7 @@ def self.create(name)
   
 end
    def self.find_or_create_by_name(name)
-    self.find(name) ?
+    self.find(name) ? self.find(name) : self.create(name)
   end
 
    def print_songs
