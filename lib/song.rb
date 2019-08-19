d72898e7665354fe3ce2b7a39f1b7aa53415dfbd
Patrick Harrison <jahman@bellatlantic.net>
2019-08-19 22:36:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,10 +12,7 @@ class Song
   end
 
   
-    song   = self.new(song_name)
-    artist = Artist.find_or_create_by_name(artist_name)
-    artist.add_song(song)
-  end
+ 
   
   def self.all
       @@all
