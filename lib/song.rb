7086db961b90a019c7c444017dd8d58576522e80
Patrick Harrison <jahman@bellatlantic.net>
2019-08-19 22:32:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,7 @@ class Song
 
    def initialize(name)
     @name = name
+    @@all<< self
   end
 
    def self.new_by_filename(filename)
@@ -12,4 +13,9 @@ class Song
     artist = Artist.find_or_create_by_name(artist_name)
     artist.add_song(song)
   end
+  
+  def self.all
+      @@all
+  end
+  
 end
